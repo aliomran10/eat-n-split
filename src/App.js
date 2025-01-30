0328ab31bafcbebdd9d771dfc52bbe2f04d9b02d
aliomran10 <125608514+aliomran10@users.noreply.github.com>
2025-01-30 23:16:26 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -69,6 +69,7 @@ export default function App() {
           selectedFriend={selectedFriend}
           friends={friends}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
